13a880f37d68704dfaa596b90f3c08d85049ab69
ChandniSingh <43898762+ChandniSingh@users.noreply.github.com>
2019-08-05 19:12:20 -0700

    Initial commit for Employee App

---
+++ EmployeeApp/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        private const string TEXT_FILE_NAME= "Employee.txt";
+        public String Name { get; set; }
+        public String Title { get; set; }
+
+        public static void WriteEmployee(Employee employee)
+        {
+            var emplyeeData = $"{employee.Name},{employee.Title}";
+            FileHelper.WriteTextFile(TEXT_FILE_NAME, emplyeeData);
+        }
+    }
+}
---
+++ EmployeeApp/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace EmployeeApp
+{
+   static class FileHelper
+    {
+        public async static void WriteTextFile(string filename,
+            string content) {
+           var storageFolder= ApplicationData.Current.LocalFolder;
+           var textFile= await storageFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+          var textstream=await textFile.OpenAsync(FileAccessMode.ReadWrite);
+           
+                var textWriter = new DataWriter(textstream);
+            textWriter.WriteString(content);
+            await textWriter.StoreAsync();
+        }
+    }
+}
